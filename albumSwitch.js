e58faa1ad8057ef668ce00d9aaa221e68a7c1d36
iisowki <marbim910@gmail.com>
2025-11-07 03:28:07 +0400

    Added comments

--- albumSwitch.js
+++ albumSwitch.js
@@ -4,21 +4,27 @@ const albumSection = document.getElementById('albumSection');
 const jsSongs = document.getElementById('jsSongs');
 const lovebombSongs = document.getElementById('lovebombSongs');
 const albumAbout = document.getElementById('albumAbout');
-const items = document.querySelectorAll('.streamplays');
 
-
+/* toggle mechanic using list*/
 lovebombAlbum.addEventListener('click', () => {
+  /* removes jsAlbum from the front */
   jsAlbum.classList.remove('front');
+  /* adds jsAlbum to the back */
   jsAlbum.classList.add('back');
+  /* removes lovebombalbum from the back */
   lovebombAlbum.classList.remove('back');
+   /* adds lovebombalbum to the front */
   lovebombAlbum.classList.add('front');
   albumSection.style.setProperty(
       '--album-bg',
       "url('assets/images/LovebombAlbumcover.jpeg')"
     );
 
+  /* hides songs under jsAlbum by adding hidden*/
   jsSongs.classList.add('hidden');
+  /* shows songs under lovebombalbum by removing hidden*/
   lovebombSongs.classList.remove('hidden');
+  /* change album title and album about */
   albumAbout.innerHTML = `
     <h1>Love Bomb</h1>
     <p>Love Bomb is a brief but potent exploration of love in its many forms, delivered with raw emotion and smooth R&B sensibilities. Across four tracks, JasonTheWeen takes listeners on a journey through the highs and lows of romance, from infatuation and longing to reflection and nostalgia.</p>
