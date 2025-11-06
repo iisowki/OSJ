34bf3ee8d6f4cdc1c340560a7e6d5a241f50e153
iisowki <marbim910@gmail.com>
2025-11-07 02:56:45 +0400

    Commit Message

---
+++ albumSwitch.js
@@ -0,0 +1,43 @@
+const jsAlbum = document.getElementById('jsAlbum');
+const lovebombAlbum = document.getElementById('lovebombAlbum');
+const albumSection = document.getElementById('albumSection');
+const jsSongs = document.getElementById('jsSongs');
+const lovebombSongs = document.getElementById('lovebombSongs');
+const albumAbout = document.getElementById('albumAbout');
+const items = document.querySelectorAll('.streamplays');
+
+
+lovebombAlbum.addEventListener('click', () => {
+  jsAlbum.classList.remove('front');
+  jsAlbum.classList.add('back');
+  lovebombAlbum.classList.remove('back');
+  lovebombAlbum.classList.add('front');
+  albumSection.style.setProperty(
+      '--album-bg',
+      "url('assets/images/LovebombAlbumcover.jpeg')"
+    );
+
+  jsSongs.classList.add('hidden');
+  lovebombSongs.classList.remove('hidden');
+  albumAbout.innerHTML = `
+    <h1>Love Bomb</h1>
+    <p>Love Bomb is a brief but potent exploration of love in its many forms, delivered with raw emotion and smooth R&B sensibilities. Across four tracks, JasonTheWeen takes listeners on a journey through the highs and lows of romance, from infatuation and longing to reflection and nostalgia.</p>
+  `;
+});
+
+jsAlbum.addEventListener('click', () => {
+  lovebombAlbum.classList.remove('front');
+  lovebombAlbum.classList.add('back');
+  jsAlbum.classList.remove('back');
+  jsAlbum.classList.add('front');
+   albumSection.style.setProperty(
+      '--album-bg',
+      "url('assets/images/JSAlbumcover.jpeg')"
+    );
+  lovebombSongs.classList.add('hidden');
+  jsSongs.classList.remove('hidden');
+  albumAbout.innerHTML = `
+    <h1>J+S</h1>
+    <p>“J+S” marks a turning point for JasonTheWeen, revealing a more personal and emotional side of his artistry. The album blends soulful melodies with experimental sounds, capturing the contrast between his on-stream energy and his quiet, introspective creativity off-camera.</p>
+  `;
+});
